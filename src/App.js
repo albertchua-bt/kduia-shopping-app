1193643b899cf5b243f97a8cae24ad555bc41381
albertchua-bt <Albert.Chua@bakertilly.com>
2023-03-10 12:38:02 -0500

    adds budget component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+import Budget from './components/Budget'
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'> 
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                     <div className='col-sm'>
                         <CartValue />
                     </div>
---
+++ src/components/Budget.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Location = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeLocation = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-secondary'> Budget: [$$] {
+      <input 
+        name="Budget" 
+        id="Budget" 
+        type='number'
+        max='20000'
+        step='10'
+        onChange={event=>changeLocation(event.target.value)}>
+      </input>	
+      }	
+    </div>
+    );
+};
+
+export default Location;
